b378868a2e6975b224c4b544e25c1ae2ceba1868
竹田勤 <takedatsutomu@taketakinnoMacBook-puro.local>
2020-02-15 15:22:35 +0900

    [Add]投稿フォーム

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,27 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
+
   end
 
   def show
+    
   end
 
   def new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    rederect_to books_path
+  end
+
   def edit
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
