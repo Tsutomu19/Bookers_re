3124d5f335174d505b0866f919cdee5f69db8501
竹田勤 <takedatsutomu@taketakinnoMacBook-puro.local>
2020-02-18 12:40:40 +0900

    0218_1N記載

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+    belongs_to :user
+    
     validates :title, presence: { message: 'can not be blank.' }
     validates :body, presence: { message: 'can not be blank.' },
                      length: { maximum: 200, message: 'is too long (maximum is 200 characters)' }
