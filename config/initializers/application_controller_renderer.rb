be681f56a9c09bc7134cb089e595d48f127767aa
竹田勤 <takedatsutomu@taketakinnoMacBook-puro.local>
2020-02-18 20:16:47 +0900

    [Add]マイページ画像反映

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f1c36d560ed479538993ad5c28f3c50e0ef549b15629863161723c555779aaf190ebb7977e5e7213dbe67ba066cea206838ea36770e3100f874b977b16bdff8b'
