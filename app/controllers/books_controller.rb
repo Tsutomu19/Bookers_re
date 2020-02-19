e99e54a0f063e060dff0cbc9d176d2c6396c0f37
竹田勤 <takedatsutomu@taketakinnoMacBook-puro.local>
2020-02-19 17:09:02 +0900

    [Add]Users

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,8 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+
+    
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   def index
-    @user = User.find(params[:id])
+    @book = Book.new
+    @user = current_user
     @users = User.all
   end
   def show
     @user = User.find(params[:id])
     @book = Book.new
     @books = @user.books
-
   end
   def edit
     @user = User.find(params[:id])
