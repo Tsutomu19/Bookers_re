a603817ea4fdc1f4218e19b86d071e496674bfbb
竹田勤 <takedatsutomu@taketakinnoMacBook-puro.local>
2020-02-20 15:24:16 +0900

    [0220]morning

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'books#top'
+  root 'homes#top'
+  get '/about', to: 'homes#about'
   devise_for :users
 resources :books
 resources :users, only: [:show, :edit, :update, :index]
